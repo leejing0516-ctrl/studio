352a306e57eca1060fc845b71e6eb10b7fad3256
李志軒 <leejing0516@gmail.com>
2025-10-05 07:03:11 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/teacher/stocks/page.tsx
+++ src/app/teacher/stocks/page.tsx
@@ -31,6 +31,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
