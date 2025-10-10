a135f433cb738dbd54cc109a290ab6035a4ecb02
李志軒 <leejing0516@gmail.com>
2025-10-10 14:26:06 +0000

    我在手機介面打開，學生端的側欄都是透明的

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,7 +61,7 @@ export default {
           '5': 'hsl(var(--chart-5))',
         },
         sidebar: {
-          DEFAULT: 'hsl(var(--sidebar-background))',
+          DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--sidebar-foreground))',
           primary: 'hsl(var(--sidebar-primary))',
           'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
