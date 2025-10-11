57b09f5cdc5eb3665f1c0049694541174c9d4830
李志軒 <leejing0516@gmail.com>
2025-10-11 03:05:20 +0000

    目前手機板上的體驗也不錯，只是點側欄的選單後，選單應該要自動收納，才不會去擋到內容，看能不能在手機版加一個動畫效果，來收闔選單

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -550,12 +550,22 @@ const SidebarMenuButton = React.forwardRef<
       size = "default",
       tooltip,
       className,
+      onClick,
       ...props
     },
     ref
   ) => {
     const Comp = asChild ? Slot : "button"
-    const { isMobile, state } = useSidebar()
+    const { isMobile, state, setOpenMobile } = useSidebar()
+    
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (onClick) {
+        onClick(event)
+      }
+      if (isMobile) {
+        setOpenMobile(false)
+      }
+    }
 
     const button = (
       <Comp
@@ -564,6 +574,7 @@ const SidebarMenuButton = React.forwardRef<
         data-size={size}
         data-active={isActive}
         className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
+        onClick={handleClick}
         {...props}
       />
     )
