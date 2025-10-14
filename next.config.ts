d210d86e9a8b4a8a21c790021116b47e3bbe1c5a
李志軒 <leejing0516@gmail.com>
2025-10-14 11:48:11 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.meee.com.tw',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
