acab9c152f3e7527b5e4f2109f31b62335e80f1a
李志軒 <leejing0516@gmail.com>
2025-10-17 05:33:31 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -22,6 +22,12 @@ const nextConfig = {
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
