3cf8e6de1068dee0c6508f3b5ab5f34ed321c39b
李志軒 <leejing0516@gmail.com>
2025-10-19 06:58:47 +0000

    你這邊沒有搞定，一切都沒用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2xl font-bold p-8">Welcome</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Welcome</h1>
     </main>
   );
 }
